d5690e78bdb24f1818858a3e8886ad7a724f76bd
roboanonymous <jsamyak591@gmail.com>
2022-08-01 13:19:44 +0530

    first commit

---
+++ src/Strings/print_string.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class print_string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Hello";
+		str = "Bye";
+		System.out.println(str);
+
+	}
+
+}
