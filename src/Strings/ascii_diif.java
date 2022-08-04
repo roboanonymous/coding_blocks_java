ba30db62e60ba085b1ee08f3c34dc8357096199b
roboanonymous <jsamyak591@gmail.com>
2022-08-04 14:30:37 +0530

    first

---
+++ src/Strings/ascii_diif.java
@@ -0,0 +1,33 @@
+package Strings;
+import java.util.*;
+public class ascii_diif {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+		String str = sc.nextLine();
+		
+		StringBuilder str2 = new StringBuilder ("");
+		int n = str.length();
+		int l = (2*n)-1;
+		int count =0;
+		for(int i=0; i<l; i++)
+		{
+			if(i%2 == 0)
+			{
+				str2.append(str.charAt(count));
+				count++;
+			}
+			else
+			{
+				int temp = str.charAt(count) - str.charAt(count-1);
+				str2.append(temp);
+			}
+		}
+		
+		String news = str2.toString();
+		System.out.println(str2);
+
+	}
+
+}
