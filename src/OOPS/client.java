f58e67a8ec1f84d09a1980e3bdef526543ca65a6
roboanonymous <jsamyak591@gmail.com>
2022-08-13 23:26:58 +0530

    first

---
+++ src/OOPS/client.java
@@ -0,0 +1,20 @@
+package OOPS;
+
+public class client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		student s = new student();
+		
+		System.out.println(s);
+		s.name = "Samyak";
+		s.age = 25;
+		
+		System.out.println(s.name);
+		System.out.println(s.age);
+		
+
+	}
+
+}
---
+++ src/OOPS/student.java
@@ -0,0 +1,7 @@
+package OOPS;
+
+public class student {
+
+	String name;
+	int age;
+}
