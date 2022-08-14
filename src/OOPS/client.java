660794e28fd2a98873d6d137258edafee7b343d6
roboanonymous <jsamyak591@gmail.com>
2022-08-14 21:24:47 +0530

    first

--- src/OOPS/client.java
+++ src/OOPS/client.java
@@ -9,11 +9,19 @@ public class client {
 		
 		System.out.println(s);
 		s.name = "Samyak";
-		s.age = 25;
+		s.age = 22;
 		
 		System.out.println(s.name);
 		System.out.println(s.age);
 		
+		
+        student s3 = new student();
+		s3.name = "A";
+		s3.age = 22;
+
+        student s4 = new student();
+		s4.name = "B";
+		s4.age = 22;
 
 	}
 
