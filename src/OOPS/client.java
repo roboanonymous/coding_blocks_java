92394cf703523e66eba088213c6f11e08229e6b5
roboanonymous <jsamyak591@gmail.com>
2022-08-14 23:29:51 +0530

    first

--- src/OOPS/client.java
+++ src/OOPS/client.java
@@ -1,6 +1,26 @@
 package OOPS;
 
 public class client {
+	
+	public static void Test1(student s1, student s2)
+	{
+		student temp = s1;
+		s1 = s2;
+		s2 = temp;
+	}
+	
+	public static void Test2(student s1, student s2)
+	{
+		int tempa = s1.age;
+		s1.age = s2.age;
+		s2.age = tempa;
+		
+		s1 = new student();
+		String tempn = s1.name;
+		s1.name = s2.name;
+		s2.name = tempn;
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -17,12 +37,20 @@ public class client {
 		
         student s3 = new student();
 		s3.name = "A";
-		s3.age = 22;
+		s3.age = 12;
 
         student s4 = new student();
 		s4.name = "B";
 		s4.age = 22;
-
+		
+		Test1(s3,s4);
+		System.out.println(s3.age + "  " + s3.name);
+		System.out.println(s4.age + "  " + s4.name);
+		
+		Test2(s3, s4);
+		System.out.println(s3.age + "  " + s3.name);
+		System.out.println(s4.age + "  " + s4.name);
+		
 	}
 
 }
