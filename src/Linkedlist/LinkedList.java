416bb38762314a97bfe01135ea1ffdbd9598754c
roboanonymous <jsamyak591@gmail.com>
2022-09-05 14:58:51 +0530

    first

--- src/Linkedlist/LinkedList.java
+++ src/Linkedlist/LinkedList.java
@@ -15,11 +15,44 @@ public class LinkedList {
 	
 	public void display()
 	{
+		System.out.println("----------------------");
 		Node temp = this.head;
 		while (temp != null)
 		{
-			System.out.println(temp.data);
+			System.out.print(temp.data + " ");
 			temp = temp.next;
 		}
+		System.out.println();
+		System.out.println("----------------------");
+	}
+	
+	
+	public void addlast (int item)
+	{
+		Node nn = new Node();
+		nn.data = item;
+		nn.next = null;
+		
+		if(this.size >= 1)
+		{
+			this.tail.next = nn;
+			
+		}
+		
+		if(this.size == 0)
+		{
+			this.head = nn;
+			this.tail = nn;
+			this.size++;
+			
+		}
+		
+		
+		else
+		{
+			this.tail = nn;
+			this.size++;
+		}
+		
 	}
 }
---
+++ src/Linkedlist/client.java
@@ -0,0 +1,19 @@
+package Linkedlist;
+
+public class client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList ll = new LinkedList ();
+		
+		ll.addlast(10);
+		ll.addlast(20);
+		ll.addlast(30);
+		ll.addlast(40);
+		ll.addlast(50);
+		
+		ll.display();
+
+	}
+
+}
