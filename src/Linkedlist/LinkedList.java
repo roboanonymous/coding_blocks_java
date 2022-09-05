8d66f77e5a883d69f2b33312a73fbd0dfa1237f2
roboanonymous <jsamyak591@gmail.com>
2022-09-05 12:42:36 +0530

    first

--- src/Linkedlist/LinkedList.java
+++ src/Linkedlist/LinkedList.java
@@ -1,5 +1,25 @@
 package Linkedlist;
 
 public class LinkedList {
-
+	
+	private class Node
+	{
+		int data;
+		Node next;
+		
+	}
+	
+	private Node head;
+	private Node tail;
+	private int size;
+	
+	public void display()
+	{
+		Node temp = this.head;
+		while (temp != null)
+		{
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+	}
 }
---
+++ src/Recursion/getpermutaion.java
@@ -0,0 +1,45 @@
+package Recursion;
+
+import java.util.*;
+
+public class getpermutaion {
+	
+	public static ArrayList <String> permutation (String str)
+	{
+		if(str.length() == 0)
+		{
+			ArrayList <String> br = new ArrayList<>();
+			br.add(" ");
+			return br;
+		}
+		
+		char ch = str.charAt(0);
+		String ros = str.substring(1);
+		
+		ArrayList <String> rr = permutation(ros);
+		ArrayList <String> mr = new ArrayList<>();
+		
+		for(String rrs: rr)
+		{
+			for(int i=0; i<rrs.length(); i++)
+			{
+				String val = rrs.substring(0,i) + ch + rrs.substring(i);
+				mr.add(val);			
+			}
+		}
+		
+		
+		
+		return mr;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	System.out.println(permutation("abc"));
+		
+
+	}
+
+}
